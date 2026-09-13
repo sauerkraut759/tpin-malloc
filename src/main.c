d5959d8db0080213070068b2d670a16e853748db
Terrapin <freq.modulada@gmail.com>
2026-09-13 11:43:25 -0300

    fix top chunk size assignment, add some helper functions and debug prints

--- src/main.c
+++ src/main.c
@@ -48,25 +48,42 @@ struct tpin_chunk {
 	struct tpin_chunk *prev;
 };
 
-struct tpin_heap{
+struct tpin_heap {
 	size_t size;
 	struct tpin_chunk *top;
 	struct tpin_chunk *free;
 };
 
-__always_inline void set_prev_in_use(struct tpin_chunk *c)
+__always_inline struct tpin_chunk *chunk_at_offset(struct tpin_chunk *c, size_t offset)
 {
-	c->size = c->size | PREV_IN_USE;
+	return (struct tpin_chunk *) ((uint8_t *)c + offset);
 }
 
-__always_inline void *chunk_to_user(struct tpin_chunk *c)
+__always_inline void set_chunk_in_use(struct tpin_chunk *c)
+{
+	struct tpin_chunk *target = chunk_at_offset(c, c->size);
+	target->size = target->size | PREV_IN_USE;
+}
+
+__always_inline void *chunk_to_mem(struct tpin_chunk *c)
 {
 	return (void *) ((uint8_t *)c + SIZE_SZ*2);
 }
 
+__always_inline struct tpin_chunk *user_to_chunk(void *mem)
+{
+	return (struct tpin_chunk *) ((uint8_t *)mem - SIZE_SZ*2);
+}
+
+__always_inline size_t get_chunk_size(struct tpin_chunk *c)
+{
+	return (c->size & ~0x15);
+}
+
 uintptr_t current_brk = 0;
 
-struct tpin_heap heap = { 0 };
+struct tpin_heap _heap= { 0 };
+struct tpin_heap *heap= &_heap;
 
 /* 
  * Add padding to requested bytes to account for chunk metadata
@@ -85,29 +102,36 @@ int malloc_init()
 
 	current_brk = (uintptr_t) ((uint8_t *)old_brk + MIN_CAPACITY);
 
-	heap.size = MIN_CAPACITY;
+	heap->size = MIN_CAPACITY;
 
 	//Set initial top
 	struct tpin_chunk *top = (struct tpin_chunk *)old_brk;
 
 	top->size = MIN_CAPACITY;
 	top->next = top->prev = NULL;
-	set_prev_in_use(top);
 
-	heap.top  = top;
-	heap.free = NULL;
+	heap->top  = top;
+	heap->free = NULL;
 }
 
 struct tpin_chunk *chunk_from_top(size_t sz)
 {
-	struct tpin_chunk *new = heap.top;
+	struct tpin_chunk *old_top = heap->top;
+	struct tpin_chunk *new_top;
+	struct tpin_chunk *new;
+
+	size_t old_size = old_top->size;
+
+	new = old_top;
 	new->size = sz;
+	new->size |= (old_size & PREV_IN_USE);
 
-	struct tpin_chunk *new_top = (struct tpin_chunk *)((uint8_t *)heap.top + sz);
-	new_top->size = heap.top->size - sz;
-	new_top->next = new_top->prev = NULL;
+	new_top = (struct tpin_chunk *)((uint8_t *)heap->top + sz);
+	new_top->size = old_size - sz;
+	new_top->next = NULL;
+	new_top->prev = new;
 
-	heap.top = new_top;
+	heap->top = new_top;
 
 	return new;
 }
@@ -116,45 +140,57 @@ void *tpin_malloc(size_t sz)
 {
 	size_t req = request_to_size(sz);
 
-	assert(req < MAX_CAPACITY && req + heap.size < MAX_CAPACITY);
-	if (req > MAX_CAPACITY || req + heap.size > MAX_CAPACITY) return NULL;
+	assert(req < MAX_CAPACITY && req + heap->size < MAX_CAPACITY);
+	if (req > MAX_CAPACITY || req + heap->size > MAX_CAPACITY) return NULL;
 
-	if (heap.size == 0) {
+	if (heap->size == 0) {
 		malloc_init();
 	}
 
 	printf("Requested bytes: %zu\n", sz);
 	printf("Returned chunk size: %zu\n", req);
-	printf("Top address: %p\n", heap.top);
+	printf("Top address: %p\n", heap->top);
+
+	printf("Looking for suitable free chunk...\n");
 
 	//check for a suitable free chunk
-	if (heap.free) {
+	if (heap->free) {
 		return NULL;
 	}
 
-	printf("---------\n");
+	printf("No suitable free chunk found, splitting top...\n");
 
 	// split top chunk
 	struct tpin_chunk *chunk = chunk_from_top(req);
 	if (!chunk) return NULL;
 	
 	printf("Returned chunk address: %p\n", chunk);
-	printf("New top address: %p\n", heap.top);
+	printf("New top address: %p\n", heap->top);
 
-	return chunk_to_user(chunk);
+	set_chunk_in_use(chunk);
+
+	printf("---------------------------\n");
+
+	return chunk_to_mem(chunk);
 }
 
 int main()
 {
 	int *xs = (int *) tpin_malloc(sizeof(int) * 10);
-	if (!xs) {
+	int *ys = (int *) tpin_malloc(sizeof(int) * 10);
+	if (!xs || !ys) {
 		printf("Memory not Allocated\n");
 		return 0;
 	}
 	for (int i = 0; i < 10; i++) {
 		xs[i] = i;
+		ys[i] = i*2;
 	}
 	for (int *ptr = xs; ptr < xs + 10; ptr++) {
+		printf("Value: %d \t Address: %p\n", *ptr, (void *)ptr);
+	}
+	printf("-----------\n");
+	for (int *ptr = ys; ptr < ys + 10; ptr++) {
 		printf("Value: %d \t Address: %p\n", *ptr, (void *)ptr);
 	}
 	return 0;
